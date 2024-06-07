923e8143b22afb6780865fa02aba82c0cd3ee881
Asi Shanmukh <varadaraju758@gmail.com>
2024-06-07 17:21:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById('input-box');
+const listContainer= document.getElementById('list-container');
+
+function addTask() {
+   if(inputBox.value === "") {
+       alert("Please enter a task");
+   } else {
+       const li = document.createElement('li');
+       li.innerHTML = inputBox.value;
+       listContainer.appendChild(li);
+       let span = document.createElement('span');
+         span.innerHTML = "\u00d7";
+         li.appendChild(span);
+   }
+   inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener('click', function(e) {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+     else if(e.target.tagName === 'SPAN') {
+     e.target.parentElement.remove(); 
+     saveData();    
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem('listContainer',listContainer.innerHTML);
+}
+
+function loadData(){
+    if(localStorage.getItem('listContainer')){
+        listContainer.innerHTML = localStorage.getItem('listContainer');
+    }
+}
+loadData();
+
+
+
+
